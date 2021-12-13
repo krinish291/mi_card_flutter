3d97c3715cdb5cca79462427b5d156b56bc0eb45
krinish291 <krinish@atliq.com>
2021-12-13 23:54:14 +0530

    Added: mi_app Fastlane android build - Version: 0.0.3 - Build: 3

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,36 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp()
   );
 }
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body:SafeArea(
+         child:  Center(
+           child: Column(
+             children: [
+               CircleAvatar(
+                 backgroundColor: Colors.yellow,
+                 radius: 50,
+                 backgroundImage: AssetImage('Images/590gv6.jpeg'),
+               ),
+               Text( 'Take It Easy',
+                 style: TextStyle(
+                     fontSize: 30,
+                     color: Colors.white
+                 ),
+               )
+             ],
+           ),
+         ),
+        ),
+      ),
+    );
+  }
+}
+
